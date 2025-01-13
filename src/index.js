16877f84eaa10e6eca800afbd38a4fd09bed9a0d
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-13 09:59:29 -0500

    notes for 1/10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+//NOTES
+//Events are user interactions with the computer (submit, click)
+//side note: it's not best practice to store all this info in the js file
+//On Monday we'll optimize and put this info in a json file
+
+
+//DOMContentLoaded is used when you ahve specific lines you want to wait to run until after the DOM is loaded
+//Otherwise just have "defer" in script in html code
+//You have to think about when you're trying to access elements
+//So if you're trying to grab an element or do something to an element, you need to wait until the DOM has finished loading
+//if you're creating a new element, you don't need to wait bc we're creating the element ourselves
+//but if you're trying to access something from the DOM (like getting an element by ID or classanme or querySelector)
+//functions and variables don't need to wait until DOM loads bc that code only executes when we call the fucntion
 const foods = [
     {
         name: "Flatburger",
@@ -51,19 +64,97 @@ const foods = [
     }
 ]
 
-const restaurantMenuElement = document.getElementById('restaurant-menu')
+function displayFoodDetails(food){ //you can call this food bc of scope - priority is within the parameter. If you call the variable it will check the function first, if it doesn't find it there, it goes one layer out, (and out again until it finds it), the global scope is the last place it will look
+    const detailImageElement = document.querySelector('.detail-image')
+    const nameElement = document.querySelector('.name') 
+    const descriptionDisplayElement = document.getElementById('description-display')
+    
+    detailImageElement.src = food.image
+    nameElement.textContent = food.name
+    descriptionDisplayElement.textContent = food.description
+}
 
-foods.forEach(food => {
+function addFoodImageToRestaurantMenu(foodObject) {
     const foodImageElement = document.createElement('img')
-    foodImageElement.src = food.image
+    foodImageElement.src = foodObject.image
+
+    foodImageElement.addEventListener("click", () => {
+        displayFoodDetails(foodObject)
+    })
+    const restaurantMenuElement = document.getElementById('restaurant-menu')
     restaurantMenuElement.appendChild(foodImageElement)
+}
+
+
+//this will be on the final challenge/assessment (DOMContentLoaded)
+document.addEventListener('DOMContentLoaded', () => {
+    //addEventListener are methods (functions that belong to an object) - tells an element to listen for an event and do something when the event occurs
+    foods.forEach(addFoodImageToRestaurantMenu)
+
+    displayFoodDetails(foods[0]) // will set the default main picture and description to the first burger
+
+    const newFoodFormElement = document.getElementById("new-food")
+    newFoodFormElement.addEventListener("submit", (event) => { //for every eventListener pass in string of event and the callback function
+    event.preventDefault() //call .preventDefault on the parameter event (an event object), so that it stops the refresh
+    // it would be bad for the page to refresh right now because you won't be able to persist data (we would lose the food/info submitted) -- but we'll cover this later
+    const newNameInputElement = document.getElementById('new-name')
+    const newImageInputElement = document.getElementById('new-image')
+    const newDescriptionTextareaElement = document.getElementById('new-description')
+
+    const newFood = { //setting the variable to an object so it matches the objects that are already in the foods array
+        name: newNameInputElement.value,
+        image: newImageInputElement.value,
+        description: newDescriptionTextareaElement.value
+    }
+// looks very similar to the forEach() .... 
+//commented out bc now all we have to do is pass it to the newFoodObject function
+    // const foodImageElement = document.createElement('img')
+    // foodImageElement.src = newFood.image
+
+    // foodImageElement.addEventListener('click', () => {
+    //     displayFoodDetails(newFood)
+    // })
+
+    // restaurantMenuElement.appendChild(foodImageElement)
+
+
+    // console.log(newDescriptionTextareaElement)
+    // console.log(foods)
+    // foods.push(newFood)
+    // console.log(foods)
+    addFoodImageToRestaurantMenu(newFood)
+
+})
 })
 
-const detailImageElement = document.querySelector('.detail-image')
-detailImageElement.src = foods[0].image
+//Instead of the foods.for each a few lines down, we could do this ...
 
-const nameElement = document.querySelector('.name') 
-nameElement.textContent = foods[0].name
 
-const descriptionDisplayElement = document.getElementById('description-display')
-descriptionDisplayElement.textContent = foods[0].description
+//Deliverable 1
+// foods.forEach(food => { //could also be done with the for...of // could also do the same thing with .map (it would just also give you some additional info - would return an array of whatever you set (like an array of the food names if you put return food.name))
+//     // const foodImageElement = document.createElement('img') //creating img elements
+//     // foodImageElement.src = food.image //setting the src of each image to the image link from the foods array
+//     addFoodImageToRestaurantMenu(food) // deleted all other code bc now we can just call the function AddFoodImagetoRestaraurantMenu on each of the foods in the foods array
+//     // foodImageElement.addEventListener("click", () => { //add the event and then the event handler (which is a callback function) - anonymous arrow function works well here // you don't need to have a parameter for this one (like you do for submitting a form) bc it doesn't have a problematic default
+    //     displayFoodDetails(food)
+    //     //Next few lines were deleted bc redundant and displayFoodDetails function was created/called bc more efficient
+    //     // detailImageElement.src = food.image //the parameter "food" changes each cycle and the .image will access the image/src for each image
+    //     // nameElement.textContent = food.name //will access the name for each food
+    //     // descriptionDisplayElement.textContent = food.description
+    // }) 
+    // restaurantMenuElement.appendChild(foodImageElement)
+
+//"DRY" - don't repeat yourself
+
+
+//Deliverable 2
+//NOTES ON SUBMIT EVENT
+//Adding a new food to be added to the page when the user submits it on form
+//There is default behavior that happens with a form and if we want it to not happen we have to prevent that
+//want to make something happen when the user clicks the submit button
+//but want to add the event listener to the form because XXXX
+
+
+//if an element doesn't have text content, you can get what is inside with element.value (so like for an input element or an img)
+
+//side note: <input type="password" --- instead of "text" --- will make the text you type just dots on screen
