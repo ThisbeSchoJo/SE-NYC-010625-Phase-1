3353b3388e821744caa0c2aaf8dc4be41c74fa2f
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-22 10:00:29 -0500

    notes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const restaurantMenu = document.getElementById('restaurant-menu')
 const foodDetailImageElement = document.querySelector('.detail-image')
 const foodNameElement = document.querySelector('.name')
 const foodDescriptionDisplayElement = document.getElementById('description-display')
+const cryptocurrencyList = document.getElementById('cryptocurrency-list')
+
 
 function addFoodImageToRestaurantMenu(food){
     const imgElement = document.createElement('img')
@@ -34,6 +36,12 @@ function displayFoodDetails(food){
     foodDescriptionDisplayElement.textContent = food.description
 }
 
+function addCryptocurrencyToList(cryptocurrency) {
+    const liElement = document.createElement('li')
+    liElement.textContent = `${cryptocurrency.name} (${cryptocurrency.symbol}): Rank #${cryptocurrency.rank}`
+    cryptocurrencyList.appendChild(liElement)
+}
+
 fetch('http://localhost:3000/foods')
 .then(response => response.json())
 .then(foods => {
@@ -42,4 +50,49 @@ fetch('http://localhost:3000/foods')
     foods.forEach(addFoodImageToRestaurantMenu)
 })
 
-// write your code here
+fetch('http://api.coincap.io/v2/assets') //Don't need to tell method bc it's a GET request
+.then(response => {
+    if (response.ok){
+        response.json().then(apiData => {
+            const cryptocurrencyArray = apiData.data
+            // apiData.data.forEach(cryptocurrency => {
+            //     if(Number(cryptocurrency.rank <= 10) { //can use Number() because we have integers, otherwise would do parseInt()
+            //         const liElement = document.createElement('li')
+            //         liElement.textContent = `${cryptocurrency.name} (${cryptocurrency.symbol}): Rank #${cryptocurrency.rank}`
+            //         const cryptocurrencyList = document.getElementById('cryptocurrency-list')
+            //         cryptocurrencyList.appendChild(liElement)
+            //     }
+            // })
+            // const filteredCryptocurrencies = apiData.data.filter(cryptocurrency => {
+            //     return Number(cryptocurrency.rank <= 10)
+            // })
+            cryptocurrencyArray.forEach(addCryptocurrencyToList)
+            
+
+            const cryptocurrencySelectElement = document.getElementById("cryptocurrency-select")
+            cryptocurrencySelectElement.addEventListener('change', () => {
+                cryptocurrencyList.innerHTML = ""
+                if (cryptocurrencySelectElement.value === 'all'){
+                    cryptocurrencyArray.forEach(addCryptocurrencyToList)
+                }
+                else if (cryptocurrencySelectElement.value === 'less'){
+                    cryptocurrencyArray.forEach(cryptocurrency => { //.forEach is used just to do something and not to get a return value (if you want/need a return value then you could choose to use map or filter or something)
+                        if(Number(cryptocurrency.rank) <= 50){
+                            addCryptocurrencyToList(cryptocurrency)
+                        }
+                    })
+                }
+                else if (cryptocurrencySelectElement.value === 'greater'){
+                    cryptocurrencyArray.forEach(cryptocurrency => { //.forEach is used just to do something and not to get a return value (if you want/need a return value then you could choose to use map or filter or something)
+                        if(Number(cryptocurrency.rank) > 50){
+                            addCryptocurrencyToList(cryptocurrency)
+                        }
+                    })
+                }
+            })
+        })
+    }
+    else{
+        alert("Error: Unable to retrieve crypto currency data!")
+    }
+})
